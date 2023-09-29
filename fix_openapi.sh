8a581436c7d32fe663ab629a8b32dea682124a6e
Michael Heap <m@michaelheap.com>
2023-09-29 17:27:59 +0100

    Fix OpenAPI script

--- fix_openapi.sh
+++ fix_openapi.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-yq '.components.schemas.InvalidParameters.items = { "$ref": "#/components/schemas/InvalidParameterChoiceItem" }' openapi.yaml > fixed.yaml
+#yq '.components.schemas.InvalidParameters.items = { "$ref": "#/components/schemas/InvalidParameterChoiceItem" }' openapi.yaml > fixed.yaml
+yq 'del(.components.schemas.DocumentTree.properties.children)' openapi.yaml > fixed.yaml
 mv fixed.yaml openapi.yaml
