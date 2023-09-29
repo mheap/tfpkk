d3137903c5003b6284a85a79b859c7f859fe9587
Michael Heap <m@michaelheap.com>
2023-09-29 18:13:40 +0100

    Remove examples

--- examples/resources/konnect_runtime_group/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "konnect_runtime_group" "my_runtimegroup" {
-    cluster_type = "CLUSTER_TYPE_HYBRID"
-            description = "A test runtime group for exploration."
-            labels = "{ \"see\": \"documentation\" }"
-            name = "Test Runtime Group"
-        }
